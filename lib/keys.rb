eb25f77ba2787e774806d96d501b5eeb9c46d28d
Jaquan Piper <jaquan.piper@gmail.com>
2020-03-15 16:06:58 +0000

    Done.

--- lib/keys.rb
+++ lib/keys.rb
@@ -8,6 +8,18 @@ require 'pry'
 #  grains: ["rice", "pasta"]
 # }
 
+groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
+
 def get_the_keys(groceries)
-  #code your solution here!
-end
+  groceries.keys 
+    #=> [:dairy, :vegetable, :meat, :grains]
+end 
+
+
+
+
